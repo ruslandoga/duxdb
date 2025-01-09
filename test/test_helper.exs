6063879e55b29352190c1dd0909ad80778ef87f9
ruslandoga <ruslandoga+gh@icloud.com>
2025-01-09 22:38:30 +0300

    no json, no test?

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,17 @@
-ExUnit.start()
+has_json? =
+  try do
+    :json.encode(1)
+  rescue
+    _ -> false
+  else
+    _ -> true
+  end
+
+exclude =
+  if has_json? do
+    []
+  else
+    [:json]
+  end
+
+ExUnit.start(exclude: exclude)
